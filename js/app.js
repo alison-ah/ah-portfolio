8459ca00b25e30e36d535bbce0f39973a365b0a2
Alison <alison@andersonholland.com>
2024-12-30 18:43:10 -0600

    Minor code clean up

--- js/app.js
+++ js/app.js
@@ -2,10 +2,7 @@ const d = new Date();
 let year = d.getFullYear();
 document.getElementById("currentYear").innerHTML = year;
 
-// Get the button:
 let mybutton = document.getElementById("goToTopButton");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -16,7 +13,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
