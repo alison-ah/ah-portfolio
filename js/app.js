0346e6d8da64054e217dced1cb6fa693098cc6ae
Alison <alison@andersonholland.com>
2026-03-14 17:17:40 -0500

    Updates

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("currentYear").innerHTML = year;
-
-let mybutton = document.getElementById("goToTopButton");
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
